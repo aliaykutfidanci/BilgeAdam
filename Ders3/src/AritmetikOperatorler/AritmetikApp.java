da71ae8203f2ab0ea61d7b5aace0b5925d0a2b84
ali.fidanci <aliaykutfidanci@gmail.com>
2019-02-02 11:53:50 +0300

    AritmetikOperatorler

--- Ders3/src/AritmetikOperatorler/AritmetikApp.java
+++ Ders3/src/AritmetikOperatorler/AritmetikApp.java
@@ -10,7 +10,41 @@ public class AritmetikApp {
 
         System.out.println("Merhaba ilk branch");
 
-        
+        /*
+        Arimetik operatorler    Karsilaştirma operandlari       Mantiksal Operatorler
+        +                           <                               && ve anlaminda sayi1 && sayi2 iki deger de true ise true doner
+        -                           >                               || ya da anlamina gelir
+        *                           <=                              ! degerin tersini alir dogru ise yanlis alir
+        /                           >=
+        %                           ==
+        ++ artirma                  !=
+        -- azaltma
+         */
+
+        int a=7; //a=7, b=3; seklinde yan yana da yazilabilir
+        int b=3;
+        int sonuc;
+        double bolmeSonucu, x=7;
+
+        sonuc=a+b;
+        System.out.println("toplama islemi: "+sonuc);
+
+        sonuc=a-b;
+        System.out.println("cikarma islemi: "+sonuc);
+
+        sonuc=a*b;
+        System.out.println("carpma islemi: "+sonuc);
+
+        sonuc=a/b;
+        System.out.println("bolme islemi: "+sonuc);
+
+        bolmeSonucu=x/b;
+        System.out.println("bolme islemi: "+bolmeSonucu);
+
+        sonuc=a%b;
+        System.out.println("kalan islemi (Mod alma): "+sonuc);
+
+
 
     }
 }
