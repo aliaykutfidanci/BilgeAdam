63853cdfef5bf591caec3a436db8237fa2283ea4
ali.fidanci <aliaykutfidanci@gmail.com>
2019-02-02 10:46:48 +0300

    AritmetikOperatorler

--- Ders3/src/AritmetikOperatorler/AritmetikApp.java
+++ Ders3/src/AritmetikOperatorler/AritmetikApp.java
@@ -4,6 +4,8 @@ public class AritmetikApp {
     public static void main(String[] args){
 
         //ctrl+alt+a git e ekleme islemi
+        //ctrl+shift+k push kisayolu
+        //sag alt koseden yeni branch acilabilir
         System.out.println("Merhaba ilk branch");
 
     }
