40a847b2cff428b4e5ac33aa9d375873f63b2699
ali.fidanci <aliaykutfidanci@gmail.com>
2019-02-03 14:15:25 +0300

    Odev

---
+++ Ders4/src/Odev/Odev.java
@@ -0,0 +1,10 @@
+package Odev;
+
+public class Odev {
+    public static void main(String[] args) {
+        /**
+         * AsalSayiApp
+         * Kalvyeden girilen sayının asal olup olmadığını bulan program yazılacak
+         */
+    }
+}
