ab48f265ce0d70c6ecef14f506f9c347dc14723b
ali.fidanci <aliaykutfidanci@gmail.com>
2019-02-10 14:09:36 +0300

    METHOD ORNEKLERI VE DIZILER

---
+++ Ders6/src/Diziler/DizilerForEachApp.java
@@ -0,0 +1,41 @@
+package Diziler;
+
+public class DizilerForEachApp {
+    public static void main(String[] args) {
+        /*int[] dizi={1,2,3,4,5,6,7};
+        String diziDegerleri="";
+        for (int i:dizi){
+            diziDegerleri=diziDegerleri+i+" ";
+
+        }
+        System.out.println("Dizinin değerleri: "+diziDegerleri);*/
+
+        /*char[] karakterler={'J','A','V','A'};
+
+        for (char c:karakterler){
+            System.out.print(c);
+        }*/
+
+        /*char[] karekter=new char[4];
+        karekter[0]='J';
+        karekter[1]='A';
+        karekter[2]='V';
+        karekter[3]='A';
+
+        for (char c:karekter){
+            System.out.print(c+" ");
+        }*/
+
+        /*String[] dizi={"Java","bir","teknoloji","topluluğudur."};
+
+        for (String s:dizi){
+
+            System.out.print(s+" ");
+        }*/
+
+        double[] dizi={1.0,2.4,3.14,5.0};
+        for (double d:dizi){
+            System.out.println(d);
+        }
+    }
+}
