ca37a2d07a0f6ba1379da7d59bf94e30e8d8912f
ali.fidanci <aliaykutfidanci@gmail.com>
2019-03-03 14:07:21 +0300

    DERS11

---
+++ Ders11/src/abstracSiniflar/Sekiller.java
@@ -0,0 +1,12 @@
+package abstracSiniflar;
+
+public abstract class Sekiller {
+    //abstraclar new yapilamaz
+    //extends edilerek kullanilabilir
+
+    public abstract double alanHesapla();
+
+    public double cevreHesapla() {
+        return 0;
+    }
+}
