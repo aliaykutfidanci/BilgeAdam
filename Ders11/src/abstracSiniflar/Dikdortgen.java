b6ae7e00577ababf241ba59b429c8f78d535a36d
ali.fidanci <aliaykutfidanci@gmail.com>
2019-03-03 14:07:12 +0300

    DERS11

---
+++ Ders11/src/abstracSiniflar/Dikdortgen.java
@@ -0,0 +1,12 @@
+package abstracSiniflar;
+
+public class Dikdortgen extends Sekiller {
+
+    private double en;
+    private double boy;
+
+    @Override
+    public double alanHesapla() {
+        return this.en*this.boy;
+    }
+}
